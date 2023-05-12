96ef9a4520a82932a255ba47fa89c13d2e4daa09
farid <farid@LAPTOP-5MLVI5CQ.fios-router.home>
2023-05-12 00:52:35 -0400

    My test data assignment is done

--- src/test/java/com/testsenario/Fb_login.java
+++ src/test/java/com/testsenario/Fb_login.java
@@ -1,5 +1,9 @@
 package com.testsenario;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.server.handler.SendKeys;
@@ -14,11 +18,16 @@ public class Fb_login extends commonfunctions {
 	Locators obj = new Locators();
 
 	@Test
-	public void f() throws InterruptedException {
+	public void f() throws InterruptedException, Exception {
+	Properties p = new Properties();	
+	FileInputStream fi = new FileInputStream(".\\src\\test\\resources\\testdata\\QAtestdata.properties");
+	 p.load(fi);
+	
+	
 		driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
-		sendkeysByAnyLocators(obj.fblogin_email_editbox, "abc@gmail.com");
-		sendkeysByAnyLocators(obj.fblogin_pass_editbox, "abc@123");
+		driver.get(p.getProperty("Fb_URL"));
+		sendkeysByAnyLocators(obj.fblogin_email_editbox, p.getProperty("Fb_email"));
+		sendkeysByAnyLocators(obj.fblogin_pass_editbox, p.getProperty("Fb_password"));
          clickByAnyLocators(obj.fblogin_login_button);
 		Thread.sleep(5000);
 		driver.quit();
